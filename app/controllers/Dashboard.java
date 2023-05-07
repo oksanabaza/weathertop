a53145d59a24f45c5cb2d007585e0d3273dba3f0
oksanabaza <oksana.misiura.cld@gmail.com>
2023-05-08 00:59:22 +0100

    multiple stations view implemented

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -4,6 +4,8 @@ import models.Station;
 import models.Reading;
 import play.Logger;
 import play.mvc.Controller;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dashboard extends Controller
 {
@@ -12,15 +14,22 @@ public class Dashboard extends Controller
 
     Reading r1 = new Reading(800, 0.5,3.5,1001);
     Reading r2 = new Reading(600,6.0, 2,1004);
+    Station s1 = new Station("Tramore");
+    s1.readings.add (r1);
+    s1.readings.add (r2);
+
+
     Reading r3 = new Reading(700,8.0,1,1000);
     Reading r4 = new Reading(200,0.5,3.5,999);
-    Station station = new Station("Tramore");
-    station.readings.add (r1);
-    station.readings.add (r2);
-    station.readings.add (r3);
-    station.readings.add (r4);
+    Station s2 = new Station("Вгтmore");
+    s2.readings.add (r3);
+    s2.readings.add (r4);
 
-    render ("dashboard.html", station);
+    List<Station> stations= new ArrayList<Station>();
+    stations.add(s1);
+    stations.add(s2);
+
+    render ("dashboard.html", stations);
   }
 }
 
