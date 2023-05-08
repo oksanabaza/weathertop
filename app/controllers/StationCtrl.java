94c50d03005e3dacfae09442ef416408d87a3e5a
oksanabaza <oksana.misiura.cld@gmail.com>
2023-05-08 17:37:03 +0100

    StationCtrl class with controller and view added

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import models.Station;
 import models.Reading;
+import play.Logger;
 import play.mvc.Controller;
 
 public class StationCtrl extends Controller
 {
     public static void index(Long id)
     {
-        render("station.html");
+        Station station = Station.findById(id);
+        Logger.info ("Station id = " + id);
+        render("station.html", station);
     }
 }
