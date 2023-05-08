75214e02dd025931a77d2eb0477f242176db8932
oksanabaza <oksana.misiura.cld@gmail.com>
2023-05-08 03:10:50 +0100

    loading from database added

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,35 +1,21 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import models.Station;
 import models.Reading;
 import play.Logger;
 import play.mvc.Controller;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Dashboard extends Controller
 {
-  public static void index() {
-    Logger.info("Rendering Dashboard");
+  public static void index()
+  {
+    Logger.info("Rendering Admin");
 
-    Reading r1 = new Reading(800, 0.5,3.5,1001);
-    Reading r2 = new Reading(600,6.0, 2,1004);
-    Station s1 = new Station("Tramore");
-    s1.readings.add (r1);
-    s1.readings.add (r2);
-
-
-    Reading r3 = new Reading(700,8.0,1,1000);
-    Reading r4 = new Reading(200,0.5,3.5,999);
-    Station s2 = new Station("Вгтmore");
-    s2.readings.add (r3);
-    s2.readings.add (r4);
-
-    List<Station> stations= new ArrayList<Station>();
-    stations.add(s1);
-    stations.add(s2);
-
+    List<Station> stations = Station.findAll();
     render ("dashboard.html", stations);
   }
 }
-
