dfa025e66b86739b26b0db05417714f7d3cc70b9
oksanabaza <oksana.misiura.cld@gmail.com>
2023-05-16 20:53:55 +0100

    init

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -12,13 +12,52 @@ import play.mvc.Controller;
 
 public class Dashboard extends Controller
 {
-  public static void index()
-  {
+  public static void index() {
     Logger.info("Rendering Admin");
     Member member = Accounts.getLoggedInMember();
     List<Station> stations = member.stations;
-    render ("dashboard.html", stations);
+
+    for (Station station : stations) {
+      List<Reading> readings = station.readings;
+      int numReadings = readings.size();
+
+      if (numReadings >= 3) {
+        Reading lastReading = readings.get(numReadings - 1);
+        Reading secondLastReading = readings.get(numReadings - 2);
+        Reading thirdLastReading = readings.get(numReadings - 3);
+
+        if (lastReading.temperature > secondLastReading.temperature && secondLastReading.temperature > thirdLastReading.temperature) {
+          station.trend = "Rising";
+        } else if (lastReading.temperature < secondLastReading.temperature && secondLastReading.temperature < thirdLastReading.temperature) {
+          station.trend = "Dropping";
+        } else {
+          station.trend = "Unchanged";
+        }
+      } else {
+        station.trend = "N/A";
+      }
+
+      // Find maximum and minimum temperature
+      double maxTemp = Double.MIN_VALUE;
+      double minTemp = Double.MAX_VALUE;
+
+      for (Reading reading : readings) {
+        double temperature = reading.temperature;
+        if (temperature > maxTemp) {
+          maxTemp = temperature;
+        }
+        if (temperature < minTemp) {
+          minTemp = temperature;
+        }
+      }
+
+      station.maxTemperature = maxTemp;
+      station.minTemperature = minTemp;
+    }
+
+    render("dashboard.html", stations);
   }
+
   public static void deleteStation (Long id)
   {
     Logger.info("Deleting a Playlist");
--- app/models/Station.java
+++ app/models/Station.java
@@ -15,10 +15,16 @@ public class Station extends Model
     public String name;
     @OneToMany(cascade = CascadeType.ALL)
     public List<Reading> readings = new ArrayList<Reading>();
+    public String trend;
+    public double maxTemperature;
+    public double minTemperature;
 
     public Station(String name)
     {
         this.name = name;
+        this.trend = trend;
+        this.maxTemperature = maxTemperature;
+        this.minTemperature = minTemperature;
     }
 }
 
