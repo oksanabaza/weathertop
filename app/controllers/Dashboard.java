8328b297f992959a160183739eb28357cc4e3c9c
oksanabaza <oksana.misiura.cld@gmail.com>
2023-05-16 21:55:43 +0100

    trending info added

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -12,13 +12,109 @@ import play.mvc.Controller;
 
 public class Dashboard extends Controller
 {
-  public static void index()
-  {
+  public static void index() {
     Logger.info("Rendering Admin");
     Member member = Accounts.getLoggedInMember();
     List<Station> stations = member.stations;
-    render ("dashboard.html", stations);
+
+    for (Station station : stations) {
+      List<Reading> readings = station.readings;
+      int numReadings = readings.size();
+
+      if (numReadings >= 3) {
+        Reading lastReading = readings.get(numReadings - 1);
+        Reading secondLastReading = readings.get(numReadings - 2);
+        Reading thirdLastReading = readings.get(numReadings - 3);
+
+        if (lastReading.temperature > secondLastReading.temperature && secondLastReading.temperature > thirdLastReading.temperature) {
+          station.trend = "Rising";
+        } else if (lastReading.temperature < secondLastReading.temperature && secondLastReading.temperature < thirdLastReading.temperature) {
+          station.trend = "Dropping";
+        } else {
+          station.trend = "Unchanged";
+        }
+      } else {
+        station.trend = "N/A";
+      }
+      ////wind trends
+      if (numReadings >= 3) {
+        Reading lastReading = readings.get(numReadings - 1);
+        Reading secondLastReading = readings.get(numReadings - 2);
+        Reading thirdLastReading = readings.get(numReadings - 3);
+
+        if (lastReading.windSpeed > secondLastReading.windSpeed && secondLastReading.windSpeed > thirdLastReading.windSpeed) {
+          station.trend = "Rising";
+        } else if (lastReading.windSpeed < secondLastReading.windSpeed && secondLastReading.windSpeed < thirdLastReading.windSpeed) {
+          station.windTrend = "Dropping";
+        } else {
+          station.windTrend = "Unchanged";
+        }
+      } else {
+        station.windTrend = "N/A";
+      }
+      /////
+      if (numReadings >= 3) {
+        Reading lastReading = readings.get(numReadings - 1);
+        Reading secondLastReading = readings.get(numReadings - 2);
+        Reading thirdLastReading = readings.get(numReadings - 3);
+
+        if (lastReading.pressure > secondLastReading.pressure && secondLastReading.pressure > thirdLastReading.pressure) {
+          station.trend = "Rising";
+        } else if (lastReading.pressure < secondLastReading.pressure && secondLastReading.pressure < thirdLastReading.pressure) {
+          station.pressureTrend = "Dropping";
+        } else {
+          station.pressureTrend = "Unchanged";
+        }
+      } else {
+        station.pressureTrend = "N/A";
+      }
+      // Find maximum and minimum values for temperature, wind speed, and pressure
+      double maxTemperature = Double.MIN_VALUE;
+      double minTemperature = Double.MAX_VALUE;
+      double maxWindSpeed = Double.MIN_VALUE;
+      double minWindSpeed = Double.MAX_VALUE;
+      double maxPressure = Double.MIN_VALUE;
+      double minPressure = Double.MAX_VALUE;
+
+      for (Reading reading : readings) {
+        double temperature = reading.temperature;
+        double windSpeed = reading.windSpeed;
+        double pressure = reading.pressure;
+
+        if (temperature > maxTemperature) {
+          maxTemperature = temperature;
+        }
+        if (temperature < minTemperature) {
+          minTemperature = temperature;
+        }
+
+        if (windSpeed > maxWindSpeed) {
+          maxWindSpeed = windSpeed;
+        }
+        if (windSpeed < minWindSpeed) {
+          minWindSpeed = windSpeed;
+        }
+
+        if (pressure > maxPressure) {
+          maxPressure = pressure;
+        }
+        if (pressure < minPressure) {
+          minPressure = pressure;
+        }
+      }
+
+      station.maxTemperature = maxTemperature;
+      station.minTemperature = minTemperature;
+      station.maxWindSpeed = maxWindSpeed;
+      station.minWindSpeed = minWindSpeed;
+      station.maxPressure = maxPressure;
+      station.minPressure = minPressure;
+    }
+
+    render("dashboard.html", stations);
   }
+
+
   public static void deleteStation (Long id)
   {
     Logger.info("Deleting a Playlist");
--- app/models/Station.java
+++ app/models/Station.java
@@ -15,10 +15,28 @@ public class Station extends Model
     public String name;
     @OneToMany(cascade = CascadeType.ALL)
     public List<Reading> readings = new ArrayList<Reading>();
+    public String trend;
+    public double maxTemperature;
+    public double minTemperature;
+    public String windTrend;
+    public String pressureTrend;
+    public double maxWindSpeed;
+    public double minWindSpeed;
+    public double maxPressure;
+    public double minPressure;
 
     public Station(String name)
     {
         this.name = name;
+        this.trend = trend;
+        this.maxTemperature = maxTemperature;
+        this.minTemperature = minTemperature;
+        this.maxWindSpeed = maxWindSpeed;
+        this.minWindSpeed = minWindSpeed;
+        this.maxPressure =maxPressure;
+        this.minPressure=minPressure;
+        this.windTrend = windTrend;
+        this.pressureTrend = pressureTrend;
     }
 }
 
